22d92411b0598e99d5a4fc8082ea26b82d7b275f
十方空烬 <xe333@sina.com>
2025-12-26 08:05:10 +0800

    合并远程仓库的初始提交

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,30 @@
-// @ts-check
-
+import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://blog.widechaos.cn',
+  base: '/',
+  output: 'static',
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  }),
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'github-dark'
+    }
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        external: []
+      }
+    }
+  }
 });
